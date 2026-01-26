562acfbc332f7ceb53bfce53bb784db1ad37f80c
pierotm <pierotarazona822@gmail.com>
2026-01-26 07:00:22 -0500

    Initial Spring Boot project setup for rutas-ods backend

---
+++ src/main/java/pe/gob/sunass/rutasods/RutasOdsBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.sunass.rutasods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutasOdsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutasOdsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/gob/sunass/rutasods/RutasOdsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.sunass.rutasods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutasOdsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
